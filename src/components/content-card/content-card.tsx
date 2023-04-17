fa36d89f04f03a046524ecb078ca3b0ef8851c11
Veronika Morgan <veranikamorgun@yandex.ru>
2023-04-17 15:56:10 +0300

    add cards

---
+++ src/components/content-card/content-card.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import cardStyles from './content-card.module.scss';
+
+interface IContentCard {
+  children: React.ReactNode
+  width?: string
+  height?: string
+  color?: string
+  title?: string
+}
+const ContentCard: FC<IContentCard> = ({ children, width, height, color, title }) => {
+  return (
+    <div className={cardStyles.card} style={{ width: width ? width : 'fit-content', height: height ? height : '50%', borderColor: color ? `var(${color})` : 'inherit' }}>
+      <div className={cardStyles.card__wrapper}>
+        {title &&
+          <h2 className={cardStyles.card__heading} style={{ color: color ? `var(${color})` : 'inherit' }}>{title}</h2>
+        }
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default ContentCard
+
